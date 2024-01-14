e2e679b9aa72cc8b7c9f454950250015f2e5e4e3
antonii1357 <antonii.spam@gmail.com>
2024-01-14 12:23:15 -0600

    Added small css fixes and show desktop jsx

---
+++ src/components/Apps/Apps.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import './apps.css'
+
+export default function Taskbar() { 
+    return <div className="apps">
+        
+    </div>
+}
--- src/components/taskBar/taskbar.jsx
+++ src/components/taskBar/taskbar.jsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from 'react'
 import './taskbar.css'
 
 export default function Taskbar() {
-    const [ time, setTime ] = useState(0),
-        [date, setDate] = useState(0)
-    function updateTime() {
+	const [time, setTime] = useState(0),
+		[date, setDate] = useState(0)
+	function updateTime() {
 		// Create a new date object
 		const now = new Date()
 
@@ -17,24 +17,30 @@ export default function Taskbar() {
 		const dateOptions = {day: '2-digit', month: '2-digit', year: 'numeric'}
 		// Format the date string based on the user's locale
 		const dateString = now.toLocaleDateString('en-GB', dateOptions)
-        
-        setTime(timeString)
-        setDate(dateString)
-    }
-    useEffect(() => {
+
+		setTime(timeString)
+		setDate(dateString)
+	}
+	useEffect(() => {
 		const intervalId = setInterval(() => {
 			updateTime()
 		}, 1000)
 		return () => clearInterval(intervalId)
-    }, [])
-    
-    return <div className="taskbar">
-            <div className="start">
-                <img src="https://cdn.pixabay.com/photo/2021/10/17/14/47/windows-7-logo-6718525_640.png" alt=""/>
+	}, [])
+
+	return (
+		<div className='taskbar'>
+			<div className='start'>
+				<img src='https://cdn.pixabay.com/photo/2021/10/17/14/47/windows-7-logo-6718525_640.png' alt='' />
             </div>
-            <div className="time">
-                <p>{time}</p>
-                <p>{date}</p>
+            <div className="right-side">
+
+			<div className='time'>
+				<p>{time}</p>
+				<p>{date}</p>
+                </div>
+                <div className="show-desktop"></div>
             </div>
-        </div>
+		</div>
+	)
 }
